ffbe427476f65eeb4bd8762ab45972b09b0808a7
naitikchau <104361541+naitikchau@users.noreply.github.com>
2022-10-05 13:47:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+Peter_pan_song="";
+Harry_potter_theme_song="";
+
+
+function setup(){
+    canvas = createCanvas(500,400);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function preload(){
+    Peter_pan_song = loadSound("music2.mp3");
+    Harry_potter_theme_song = loadSound("music.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,530);
+}
